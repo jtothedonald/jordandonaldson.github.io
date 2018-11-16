56c39c4f0be4f40040607f53a6e48a34abc7a509
Jordan Donaldson <hockeyfan729@ymail.com>
2018-11-16 10:16:27 -0800

    Database Fix
    Yep

--- js/addData.js
+++ js/addData.js
@@ -1,14 +1,14 @@
 const form = document.querySelector('#add-user-input');
 
-form.addEventListener('submit', function(evt) {
-  evt.preventDefault();
+function sendFormData() {
   db.collection('userInput').add({
-    createdAt: firebase.firestore.FieldValue.serverTimestamp(),
     name: form.name.value,
     email: form.email.value,
+    phone: form.phone.value,
     message: form.message.value
   });
   form.name.value = '';
   form.email.value = '';
+  form.phone.value = '';
   form.message.value = '';
-});
+}
